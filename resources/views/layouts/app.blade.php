236f9e7ace2698912ec53c2470426a98331131aa
Kelvin Macharia <ngunyimacharia@gmail.com>
2018-12-01 12:48:43 +0000

    Favicon fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,9 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Childsafe</title>
+
+    <link rel="shortcut icon" type="image/png" href="/images/child16.png"/>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
